23fd3315c8a7fd09965af80bf9dcedac618c4aac
Jason-csc <yyzjason@umich.edu>
2022-04-05 00:45:35 -0400

    streaming TDW images

---
+++ tdwENV/views/TDW.py
@@ -0,0 +1,153 @@
+'''
+Demo for magnet robot in TDW
+'''
+
+
+from tdw.controller import Controller
+from tdw.tdw_utils import TDWUtils
+from tdw.add_ons.third_person_camera import ThirdPersonCamera
+# from tdw.add_ons.image_capture import ImageCapture
+from magnebot import Magnebot, ActionStatus, Arm
+from tdw.librarian import ModelLibrarian
+from tdwENV.views.imagecapture import ImgCaptureModified
+
+import numpy as np
+import numpy
+import cv2
+
+
+
+# # show all the objects in TDW
+# librarian = ModelLibrarian()
+# for record in librarian.records:
+#     print(record.name)
+
+
+def startTDW(imgQueue,prepared):
+    c = Controller()
+    magnebot = Magnebot(position={"x": 0, "y": 0, "z": -1.67}, rotation={"x": 0, "y": 0, "z": 0},robot_id=c.get_unique_id())
+    magnebot2 = Magnebot(position={"x": 0, "y": 0, "z": 1.67}, rotation={"x": 0, "y": 180, "z": 0},robot_id=c.get_unique_id())
+    # Create a camera and enable image capture.
+    camera = ThirdPersonCamera(position={"x": 0, "y": 1.5, "z": -1.55},
+                            rotation={"x": 30, "y": 0, "z": 0},
+                            field_of_view = 100,
+                            avatar_id="a")
+    # path: directory to store images
+
+    capture = ImgCaptureModified(avatar_ids=["a"],png=False,image_q=imgQueue)
+    # Note the order of add-ons. The Magnebot must be added first so that the camera can look at it.
+    c.add_ons.extend([magnebot, magnebot2, camera, capture])
+    # c.add_ons.extend([magnebot])
+
+
+    print("Setting Up Scene...")
+    '''Set up scene'''
+    commands = [{"$type": "set_screen_size",
+                    "width": 500,
+                    "height": 500}]
+    commands.extend([TDWUtils.create_empty_room(12, 12)])
+    commands.extend(c.get_add_physics_object(model_name='quatre_dining_table',
+                                        #  library="models_core.json",
+                                            position={"x": 0, "y": 0, "z": 0},
+                                            object_id=c.get_unique_id()))
+
+    bowl_id = c.get_unique_id()
+    commands.extend(c.get_add_physics_object(model_name='serving_bowl',
+                                        library="models_core.json",
+                                            position={"x": -0.3, "y": 0.85, "z": -1},
+                                            rotation={"x":0,"y":120,"z":0},
+                                            mass = 3,
+                                            bounciness=1,
+                                            static_friction = 1,
+                                            object_id=bowl_id,
+                                            ))
+
+    apple_id = c.get_unique_id()
+    commands.extend(c.get_add_physics_object(model_name='apple',
+                                        library="models_core.json",
+                                            position={"x": 0.2, "y": 0.8682562, "z": -1.1},
+                                            mass = 3,
+                                            bounciness=1,
+                                            object_id=apple_id))
+
+
+    orange_id = c.get_unique_id()
+    commands.extend(c.get_add_physics_object(model_name='b04_orange_00',
+                                        library="models_core.json",
+                                            position={"x": 0.10, "y": 0.8682562, "z": -0.9},
+                                            mass = 3,
+                                            bounciness=1,
+                                            object_id=orange_id))
+
+
+    banana_id = c.get_unique_id()
+    commands.extend(c.get_add_physics_object(model_name='b04_banana',
+                                        library="models_core.json",
+                                            position={"x": 0, "y": 0.8682562, "z": -0.4},
+                                            mass = 3,
+                                            bounciness=1,
+                                            object_id=banana_id))
+
+    banana2_id = c.get_unique_id()
+    commands.extend(c.get_add_physics_object(model_name='b04_banana',
+                                        library="models_core.json",
+                                            position={"x": 0.24, "y": 0.8682562, "z": 0.4},
+                                            mass = 3,
+                                            bounciness=1,
+                                            object_id=banana2_id))
+
+
+    apple2_id = c.get_unique_id()
+    commands.extend(c.get_add_physics_object(model_name='apple',
+                                        library="models_core.json",
+                                            position={"x": -0.17, "y": 0.8682562, "z": 0.25},
+                                            mass = 3,
+                                            bounciness=1,
+                                            object_id=apple2_id))
+
+    resp = c.communicate(commands)
+
+
+
+    print("Starting TDW...")
+    prepared[0] = True
+
+    '''Control the robot by pick/drop'''
+    while True:
+        cmd = input(">>Input Command: ")
+        if cmd == "pick apple":
+            magnebot.grasp(apple_id,Arm.right)
+            while magnebot.action.status == ActionStatus.ongoing:
+                c.communicate([])
+            c.communicate([])
+            # print(magnebot.action.status)
+            magnebot.reach_for(target={"x": 0.1, "y": 1.3, "z": -1}, arm=Arm.right)
+            while magnebot.action.status == ActionStatus.ongoing:
+                c.communicate([])
+            c.communicate([])
+            print(magnebot.action.status)
+        elif cmd == "drop":
+            magnebot.reach_for(target={"x": -0.22, "y": 1.25, "z": -1}, arm=Arm.right)
+            while magnebot.action.status == ActionStatus.ongoing:
+                c.communicate([])
+            c.communicate([])
+            magnebot.drop(apple_id,Arm.right)
+            while magnebot.action.status == ActionStatus.ongoing:
+                c.communicate([])
+            c.communicate([])
+            # print(magnebot.action.status)
+            magnebot.reach_for(target={"x": 0, "y": 1.2, "z": -1}, arm=Arm.right)
+            while magnebot.action.status == ActionStatus.ongoing:
+                c.communicate([])
+            c.communicate([])
+            # print(magnebot.action.status)
+            magnebot.reset_arm(arm=Arm.right)
+            while magnebot.action.status == ActionStatus.ongoing:
+                c.communicate([])
+            c.communicate([])
+            print(magnebot.action.status)
+        else:
+            c.communicate({"$type": "terminate"})
+            break
+
+
--- tdwENV/views/index.py
+++ tdwENV/views/index.py
@@ -11,31 +11,37 @@ import threading
 import numpy as np
 import time
 
+from tdwENV.views.TDW import startTDW
+
+
 import cv2
 
 camera=[]
+prepared = [False]
 prev = None
 
 @tdwENV.app.route('/')
 def show_index():
     """Display / route."""
     context = {}
-    thread = threading.Thread(target=out,args=(camera,))
+    thread = threading.Thread(target=startTDW,args=(camera,prepared,))
     thread.start()
+
+    while True:
+        if prepared[0]:
+            break
+        time.sleep(0.1)
 
     return flask.render_template("index.html", **context)
 
 
 
-def out(c):
-    i = 0
-    while True:
-        i += 1
-        img= (np.random.randint(0,255,(1000,1000,3))).astype(np.uint8)
-        c.append(img)
-        if i > 100:
-            time.sleep(2)
-            i = i // 2
+# def out(c):
+#     i = 0
+#     while True:
+#         i += 1
+#         img= (np.random.randint(0,255,(500,20,3))).astype(np.uint8)
+#         c.append(img)
 
 
 def generate_frames():
@@ -47,11 +53,11 @@ def generate_frames():
         else:
             frame=camera.pop(0)
             prev = frame
-        ret,buffer=cv2.imencode('.png',frame)
+        ret,buffer=cv2.imencode('.jpg',frame)
         frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
-                   b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @tdwENV.app.route('/video')
